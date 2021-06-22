0d8494336dd50e8c14b53b6e6d978776d1d29c04
Truong Thanh Nhut <thanhnhut.02jan@gmail.com>
2021-06-22 17:23:30 +0700

    Update ChatBoxCard

--- src/component/Sidebar/ChatboxCard.jsx
+++ src/component/Sidebar/ChatboxCard.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 import Avatar from '../common/Avatar';
 
-const ChatboxCard = () => {
+const ChatboxCard = (props) => {
+  const {
+    username = "Thanh Nhut",
+    lastMsg = "Lorem ipsum dolor,adfadf asdfasfafasfsafasfasdfs Tui laf"
+
+  } = props;
+
   return (
     <div className="chatbox-card">
       <Avatar size="large" />
+      <div className="chatbox-card__info">
+        <span style={{ display: "block", fontWeight: "600" }}>{username}</span>
+        <span>{lastMsg}</span>
+      </div>
     </div>
   );
 }
