75578a9d64fff58b11b635178e24b56f1563e0c6
Truong Thanh Nhut <thanhnhut.02jan@gmail.com>
2021-06-24 15:37:30 +0700

    Add app routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+
 import Sidebar from './component/Sidebar';
 import Chatbox from './component/Chatbox';
+import Login from './component/Login';
 
 function App() {
   return (
     <div className="app">
-      <Sidebar />
-      <Chatbox />
+      <Router>
+        <Route path="/login">
+          <Login />
+        </Route>
+
+        <Route exact path="/">
+          <Sidebar />
+          <Chatbox />
+        </Route>
+
+      </Router>
     </div>
   );
 }
