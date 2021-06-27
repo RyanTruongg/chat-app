296930279fafe9b475a5539b45fd41665aa690be
Truong Thanh Nhut <thanhnhut.02jan@gmail.com>
2021-06-27 18:58:11 +0700

    Add socket listener:users

--- src/component/Sidebar/Sidebar.jsx
+++ src/component/Sidebar/Sidebar.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
-// import Avatar from '../common/Avatar';
 import SidebarHeader from './SidebarHeader';
 import ChatboxList from './ChatboxList';
+
+import socket from '../../service/websocket';
+import { useAuth } from '../../hook/use-auth';
 
 import('./sidebar.css')
 
 const Sidebar = () => {
+  const [listUser, setListUser] = useState([]);
+  const auth = useAuth();
+
+  useEffect(() => {
+    if (auth.loginState === "loged") {
+      socket.on("users", users => {
+        console.log(users);
+        console.log(auth.user?.uid)
+        users = users.filter((user) => user?.userID !== auth.user?.uid);
+        setListUser(users);
+      });
+    }
+
+    return () => {
+      socket.off("users");
+    };
+  }, [auth.loginState, auth.user?.uid]);
+
   return (
     <div className="sidebar">
-      {/* <Avatar /> */}
-      <SidebarHeader />
-      <ChatboxList />
+      <SidebarHeader providerData={auth.user?.providerData[0]} />
+      <ChatboxList listData={listUser} />
     </div>
   );
 }
