b5871c466ba75dfd850f0d229418885889731284
Truong Thanh Nhut <thanhnhut.02jan@gmail.com>
2021-07-01 09:00:56 +0700

    Config: Create source map = false

--- server/build/static/js/8.11472f54.chunk.js
+++ server/build/static/js/8.11472f54.chunk.js
@@ -1,2 +1,1 @@
 (this["webpackJsonpchat-app"]=this["webpackJsonpchat-app"]||[]).push([[8],{105:function(p,a,c){}}]);
-//# sourceMappingURL=8.11472f54.chunk.js.map
--- server/build/static/js/9.76b589af.chunk.js
+++ server/build/static/js/9.76b589af.chunk.js
@@ -1,2 +1,1 @@
 (this["webpackJsonpchat-app"]=this["webpackJsonpchat-app"]||[]).push([[9],{103:function(p,a,c){}}]);
-//# sourceMappingURL=9.76b589af.chunk.js.map
