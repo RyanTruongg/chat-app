de589303ec6e4da9089d21a9b1fe3ce2494167ee
Truong Thanh Nhut <thanhnhut.02jan@gmail.com>
2021-07-08 19:35:11 +0700

    Update toggle fullscreen

--- client/src/component/Sidebar/SidebarHeader.jsx
+++ client/src/component/Sidebar/SidebarHeader.jsx
@@ -7,13 +7,22 @@ import Button from '../common/Button';
 import { useAuth } from '../../hook/use-auth';
 
 const SidebarHeader = ({ providerData, setOpen, open }) => {
+  const [fullscreen, setFullscreen] = useState(false);
   const auth = useAuth();
   const { photoURL } = providerData || {};
 
-  function reqFullScr() {
+  function toggleFullscreen() {
     const root = document.getElementById("root");
-    if (root) {
+    if (!document.fullscreenElement) {
       root.requestFullscreen()
+        .then(() => setFullscreen(true))
+        .catch(err => {
+          alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+        });
+
+    } else {
+      setFullscreen(false);
+      document.exitFullscreen();
     }
   }
 
@@ -27,7 +36,7 @@ const SidebarHeader = ({ providerData, setOpen, open }) => {
 
         <div className="sidebar-top-right">
           <Button iconName="more_horiz" title="More" />
-          <Button iconName="fullscreen" onClick={reqFullScr} />
+          <Button iconName={fullscreen ? "fullscreen_exit" : "fullscreen"} onClick={toggleFullscreen} />
           <Button iconName="logout" title="Signout" onClick={auth.signout} />
           {
             open && <Button iconName="arrow_back" color="var(--primary)" onClick={() => setOpen(false)} />
