1c63ff82dd1897658cb9f765860fc7ce4a14054d
Truong Thanh Nhut <thanhnhut.02jan@gmail.com>
2021-07-23 13:51:04 +0700

    Add requireAuth middleware

---
+++ server/middlewares/requireAuth.js
@@ -0,0 +1,24 @@
+const admin = require('../firebase/firebaseAdmin');
+
+async function requireAuth(req, res, next) {
+  const jwt = req.get('Authorization')?.split(' ')[1];
+  if (!jwt) {
+    res.status(401).send('Unauthorized')
+    next('Unauthorized');
+  }
+  else
+    admin
+      .auth()
+      .verifyIdToken(jwt)
+      .then((decodedToken) => {
+        req.decodedToken = decodedToken;
+        next();
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(401).send('Unauthorized');
+        next(error);
+      });
+}
+
+module.exports = requireAuth;
