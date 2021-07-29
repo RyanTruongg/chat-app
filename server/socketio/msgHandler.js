cb4c98b36f033ee9c48256dc70fca6c8679eb85e
Truong Thanh Nhut <thanhnhut.02jan@gmail.com>
2021-07-29 10:07:15 +0700

    Update msgHandler

--- server/socketio/msgHandler.js
+++ server/socketio/msgHandler.js
@@ -1,7 +1,33 @@
 const Message = require("../model/Message");
+const User = require('../model/User');
+
+async function updateContacts(u1, u2, { from, content, UTC }) {
+  let contact = {
+    seen: false,
+    contactID: u2,
+    lastMsg: {
+      from: from,
+      content: content,
+      timestamp: UTC,
+    }
+  }
+
+  let options = {
+    upsert: true,
+    arrayFilters: [
+      { "receivingUser.contactID": u2 }
+    ]
+  }
+  return User.findOneAndUpdate(
+    { uid: u1 },
+    { $addToSet: { "contacts.$[receivingUser]": contact } },
+    options)
+    .exec();
+
+}
 
 module.exports = (io) => {
-  const createMsg = function (payload) {
+  const createMsg = function (payload, fn) {
     const socket = this;
     const { from, to, content } = payload;
     const UTC = Date.now();
@@ -12,13 +38,39 @@ module.exports = (io) => {
       timestamp: UTC
     })
 
-    doc.save((err, doc) => {
+    doc.save(async (err, doc) => {
       if (err) return () => {
         console.log(err)
-        socket.emit("msg:create", { success: false, ...payload });
       }
-      socket.emit("msg:create", { success: true, doc: doc });
-      socket.to(doc.to).emit("private msg", { success: true, doc: doc });
+
+      let sender = await updateContacts(from, to, doc).catch(e => console.log(e));
+      let receiver = await updateContacts(to, from, doc).catch(e => console.log(e));
+
+      let senderUpdatedContact = {
+        contactID: receiver.uid,
+        displayName: receiver.displayName,
+        photoURL: receiver.photoURL,
+        seen: true,
+        lastMsg: {
+          from: from,
+          content: content,
+          timestamp: UTC,
+        }
+      }
+      fn({ doc: doc, contact: senderUpdatedContact }) //callback from sender
+
+      let receiverUpdatedContact = {
+        contactID: sender.uid,
+        displayName: sender.displayName,
+        photoURL: sender.photoURL,
+        seen: false,
+        lastMsg: {
+          from: from,
+          content: content,
+          timestamp: UTC,
+        }
+      }
+      socket.to(doc.to).emit("private msg", { doc: doc, contact: receiverUpdatedContact }); // emit to receiver
     });
   }
 
